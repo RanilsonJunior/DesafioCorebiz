443e202416bdd0b82907c5db7da97fd518361aa5
RanilsonJunior <ranilsonjunior2@gmail.com>
2021-09-13 23:40:57 -0300

    final

---
+++ script.js
@@ -0,0 +1,43 @@
+
+/*Inicio do slidershow dos banners*/ 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+/*Fim do slidershow dos banners*/
+
+/*Inicio do slidershow dos produtos*/ 
+$(function(){
+$('.pro').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  });
+});
+
+/*Fim do slidershow dos produtos*/
